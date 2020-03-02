df5aa489eff54f04e7f6edb1abf9b448351021ea
MatthieuDartiailh <marul@laposte.net>
2020-03-02 16:44:47 -0500

    post-release

--- bytecode/__init__.py
+++ bytecode/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.11.0'
+__version__ = '0.12.0.dev'
 
 __all__ = ['Label', 'Instr', 'SetLineno', 'Bytecode',
            'ConcreteInstr', 'ConcreteBytecode',
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@
 #  - git commit -a -m "post-release"
 #  - git push
 
-VERSION = '0.11.0'
+VERSION = '0.12.0.dev'
 
 DESCRIPTION = 'Python module to generate and modify bytecode'
 CLASSIFIERS = [
