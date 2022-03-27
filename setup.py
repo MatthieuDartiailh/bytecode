63ff0cf912558421d7792ad1559b57c8d0800637
Matthieu Dartiailh <m.dartiailh@gmail.com>
2022-03-27 22:15:25 +0200

    move to a src layout
    And move the tests outside the package

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ def main():
         "maintainer": "Matthieu C. Dartiailh",
         "maintainer_email": "m.dartiailh@gmail.com",
         "classifiers": CLASSIFIERS,
-        "packages": ["bytecode", "bytecode.tests"],
+        "package_dir": {"": "src"},
+        "packages": ["src/bytecode"],
         "python_requires": ">=3.6",
     }
     setup(**options)
