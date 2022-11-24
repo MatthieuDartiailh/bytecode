a4364eec73986305037db5ac8b9dd1a6f4682da3
Matthieu Dartiailh <m.dartiailh@gmail.com>
2022-11-24 20:26:02 +0100

    tests.frameworks: remove safeties (experiment)

--- tests/frameworks/sitecustomize.py
+++ tests/frameworks/sitecustomize.py
@@ -24,14 +24,6 @@ class FunctionCollector(ModuleWatchdog):
                 new = cfg.to_code()
                 # Check we can still disassemble the code
                 dis.dis(new, file=io.StringIO())
-                # If the code does not contain dead code, check we use safe values
-                # for the stack (stacksize and exception table)
-                # In the presence of dead code we cannot reproduce CPython caluclation
-                # for the dead portions
-                if not cfg.get_dead_blocks():
-                    assert new.co_stacksize == function.__code__.co_stacksize
-                    if sys.version_info >= (3, 11):
-                        assert new.co_exceptiontable == function.__code__.co_exceptiontable
             except Exception:
                 print("Failed to recompile %s" % fname)
                 dis.dis(function)
