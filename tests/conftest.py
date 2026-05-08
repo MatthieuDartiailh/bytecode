d02b85c63edf0b19e70b77ea2d06033c31075840
Gabriele N. Tornetta <gabriele.tornetta@datadoghq.com>
2026-05-08 10:05:55 +0100

    fix linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,7 @@
-import bytecode
-
-
 def pytest_report_header():
     import importlib.util
 
     spec = importlib.util.find_spec("bytecode.concrete")
-    kind = "pure-Python" if (spec and spec.origin and spec.origin.endswith(".py")) else "Cython"
-    return f"bytecode: {kind} build ({bytecode.__file__})"
+    is_pure = spec and spec.origin and spec.origin.endswith(".py")
+    kind = "pure-Python" if is_pure else "Cython"
+    return f"bytecode: {kind} build"
